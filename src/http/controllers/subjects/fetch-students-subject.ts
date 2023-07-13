0f685044f5551398b168852488c5ab1aa7cdbf67
mayconndouglass <mayconn.douglass1414@gmail.com>
2023-07-13 15:50:27 -0300

    Implemented fetch students subject

--- src/http/controllers/subjects/fetch-students-subject.ts
+++ src/http/controllers/subjects/fetch-students-subject.ts
@@ -1,3 +1,15 @@
-/* export const fetchStudentsSubject = async () => {
+import { PrismaSubjectsRepository } from "@/repositories/prisma/prisma-subjects-repository"
+import { FetchStudentSubjectsUseCase } from "@/use-cases/subject/fetch-student-subjects"
+import { FastifyReply, FastifyRequest } from "fastify"
 
-} */
+export const fetchStudentsSubject = async (request: FastifyRequest, reply: FastifyReply) => {
+  const prismaSubjectRepository = new PrismaSubjectsRepository()
+  const fetchStudentsSubject =
+    new FetchStudentSubjectsUseCase(prismaSubjectRepository)
+
+  const subjects = await fetchStudentsSubject.execute({
+    studentId: request.user.sub
+  })
+
+  return reply.status(200).send(subjects)
+}
--- src/http/controllers/subjects/routes.ts
+++ src/http/controllers/subjects/routes.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from "fastify"
 import { registerSubject } from "./register-subject"
 import { updateSubject } from "./update-subject"
 import { removeSubject } from "./remove-subject"
+import { fetchStudentsSubject } from "./fetch-students-subject"
 
 export const subjectsRoutes = async (app: FastifyInstance) => {
   app.addHook("onRequest", verifyJwt)
@@ -10,4 +11,5 @@ export const subjectsRoutes = async (app: FastifyInstance) => {
   app.post("/subjects/register", registerSubject)
   app.patch("/subjects/update", updateSubject)
   app.delete("/subjects/delete", removeSubject)
+  app.get("/subjects/students", fetchStudentsSubject)
 }
